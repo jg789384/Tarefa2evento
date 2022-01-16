0468289e66ebb36ba9b3e51371a9f013ce416994
João Gustavo <jg789384@gmail.com>
2022-01-16 15:43:42 -0300

    first commit

---
+++ Evento.js
@@ -0,0 +1,22 @@
+let data = 21/01/2022
+
+if(data > 20/01/2022) {
+    console.log("Data válida, permitir cadastro")
+
+}else {
+    console.log("Data inválida, não é possível efetuar o cadastro")
+} 
+let idade = 18
+
+if (idade >= 18) {
+    console.log("18 anos ou mais, cadastro permitido")
+}else {
+    console.log ("Menor de 18 anos, não permitir cadastro")
+}
+
+let quantidade = 100
+if(quantidade <= 100) {
+    console.log ("100 ou menos, cadastro pertmitido")
+}else {
+    console.log ("Mais de 100 participantes, quantidade de vagas excedida.")
+}
